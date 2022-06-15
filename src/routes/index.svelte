43462b3dc918fa1dad5b6d86f19a20c754ff9b4e
Ose Ughu <zinjmyster10@gmail.com>
2022-06-15 07:13:26 +0100

    add url preview

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,20 +6,7 @@
 
 <svelte:head>
   <title>Osemudiamhen Ughu</title>
-  <meta name="description" content="Osemudiamhen Ughu, a bad-ass dev." />
-  <meta
-    name="keywords"
-    content="osemudiamhen, ughu, svelte, website developer, portfolio, personal, personal website, personal portfolio"
-  />
-  <meta
-    property="og:image"
-    content="https://res.cloudinary.com/drze4mafd/image/upload/v1637477675/Ose/Ose_jhc4fd.jpg"
-  />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="627" />
-  <meta property="og:url" content="https://oseughu.com" />
-  <meta name="author" content="Osemudiamhen Ughu" />
-  <meta name="twitter:card" content="summary" />
+
   <link rel="icon" href="/favicon.ico" />
 </svelte:head>
 
